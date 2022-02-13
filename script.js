7e38a5c20bbbbf9b412a65e3c0b9deb89c80169d
Maksim Pasnitsenko <emexpas@outlook.com>
2022-02-13 18:58:32 +0200

    version 1 of the game is done

--- script.js
+++ script.js
@@ -13,43 +13,93 @@ const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
+
+let scores, currentScore, activePlayer, playing;
+
 // Starting point
-score0El.textContent = 0;
-score1El.textContent = 0;
+const init = () => {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
 
-// Hide the dice in the beginning
-diceEl.classList.add('hidden');
+  // Hide the dice in the beginning
+  diceEl.classList.remove('hidden');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+};
+init();
+
+const switchPlayer = () => {
+  // switch to next player
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  // toggle() will add the class if it's there, if not, it will remove it
+  // was <section class="player player--0 player--active">
+  // became <section class="player player--1">
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
 
 // Rolling dice functionality
 btnRoll.addEventListener('click', function () {
-  // 1. Generating a random dice roll
-  const dice = Math.trunc(Math.random() * 6) + 1;
-  console.log(dice);
-  // 2. Display dice
-  diceEl.classList.remove('hidden');
-  diceEl.src = `dice-${dice}.png`;
-  // 3. Check for rolled 1
-  if (dice !== 1) {
-    // Add dice to current score
-    currentScore += dice;
-    // Current active player
-    document.getElementById(`current--${activePlayer}`).textContent =
-      currentScore;
-    console.log(currentScore);
-  } else {
-    // Switch to next player
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    currentScore = 0;
-    activePlayer = activePlayer === 0 ? 1 : 0;
-    // toggle() will add the class if it's there, if not, it will remove it
-    // was <section class="player player--0 player--active">
-    // became <section class="player player--1">
-    player0El.classList.toggle('player--active');
-    player1El.classList.toggle('player--active');
-    console.log(currentScore);
+  if (playing) {
+    // 1. Generating a random dice roll
+    const dice = Math.trunc(Math.random() * 6) + 1;
+    console.log(dice);
+
+    // 2. Display dice
+    diceEl.classList.remove('hidden');
+    diceEl.src = `dice-${dice}.png`;
+
+    // 3. Check for rolled 1
+    if (dice !== 1) {
+      // Add dice to current score
+      currentScore += dice;
+      // Current active player
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+      console.log(currentScore);
+    } else {
+      // Switch to next player
+      switchPlayer();
+    }
   }
 });
+
+// 1. Add current score to active player's score
+btnHold.addEventListener('click', function () {
+  if (playing) {
+    // scores[1] = scores[1] + currentScore;
+    scores[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+
+    // 2. Check if player's score is >= 100
+    if (scores[activePlayer] >= 100) {
+      // Finish the game
+      playing = false;
+      diceEl.classList.add('hidden');
+
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active');
+    } else {
+      // Switch to the next player
+      switchPlayer();
+    }
+  }
+});
+
+btnNew.addEventListener('click', init);
